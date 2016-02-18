3003b3ea3974fa1ac8e2132b934a1e8a15ee7f64
Felipe Rodríguez <f.rodriguez06@ufromail.cl>
2016-02-18 15:20:13 -0300

    V1

---
+++ platforms/android/assets/www/mostrar_respuestas.php
@@ -0,0 +1,373 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"> 
+<title>APK Prueba</title>
+</head>
+
+<body>
+<div data-role="page" id="preguntas">
+
+  <div data-role="header">
+    <h1>APK Prueba</h1>
+  </div>
+
+  <div data-role="main" class="ui-content">
+  <p>Alumno(a): Carolina Aldea</p>
+    <table border='1' class="table table-responsive">
+    	<tr>
+		<td><p>Pregunta 1(<a href="#p1">Ver Pregunta</a>)</p></td>
+        <td>AL</td>
+        </tr>
+       	
+        <tr>
+		<td><p>Pregunta 2(<a href="#p2">Ver Pregunta</a>)</p></td>
+        <td>SC</td>
+        </tr>
+        
+        <tr>
+		<td><p>Pregunta 3(<a href="#p3">Ver Pregunta</a>)</p></td>
+        <td>AL</td>
+        </tr>
+        
+        <tr>
+		<td><p>Pregunta 4(<a href="#p4">Ver Pregunta</a>)</p></td>
+        <td>MV</td>
+        </tr>
+        
+        <tr>
+		<td><p>Pregunta 5(<a href="#p5">Ver Pregunta</a>)</p></td>
+        <td>SC</td>
+        </tr>
+        
+        <tr>
+		<td><p>Pregunta 6(<a href="#p6">Ver Pregunta</a>)</p></td>
+        <td>AM</td>
+        </tr>
+        
+        <tr>
+		<td><p>Pregunta 7(<a href="#p7">Ver Pregunta</a>)</p></td>
+        <td>AL</td>
+        </tr>
+        
+        <tr>
+		<td><p>Pregunta 8(<a href="#p8">Ver Pregunta</a>)</p></td>
+        <td>SC</td>
+        </tr>
+        
+        <tr>
+		<td><p>Pregunta 9(<a href="#p9">Ver Pregunta</a>)</p></td>
+        <td>Am</td>
+        </tr>
+        
+        <tr>
+		<td><p>Pregunta 10(<a href="#p10">Ver Pregunta</a>)</p></td>
+        <td>NU</td>
+        </tr>
+        
+        <tr>
+		<td><p>Pregunta 11(<a href="#p11">Ver Pregunta</a>)</p></td>
+        <td>SC</td>
+        </tr>
+        
+        <tr>
+		<td><p>Pregunta 12(<a href="#p12">Ver Pregunta</a>)</p></td>
+        <td>MV</td>
+        </tr>
+        
+        <tr>
+		<td><p>Pregunta 13(<a href="#p13">Ver Pregunta</a>)</p></td>
+        <td>AL</td>
+        </tr>
+        
+        <tr>
+		<td><p>Pregunta 14(<a href="#p14">Ver Pregunta</a>)</p></td>
+        <td>AL</td>
+        </tr>
+        
+        <tr>
+		<td><p>Pregunta 15(<a href="#p15">Ver Pregunta</a>)</p></td>
+        <td>MV</td>
+        </tr>
+        
+        <tr>
+		<td><p>Pregunta 16(<a href="#p16">Ver Pregunta</a>)</p></td>
+        <td>SC</td>
+        </tr>
+        
+        <tr>
+		<td><p>Pregunta 17(<a href="#p17">Ver Pregunta</a>)</p></td>
+        <td>NU</td>
+        </tr>
+                 
+     </table>
+  </div>
+  
+  <div data-role="footer">
+    <h1>Proyecto FONDEF ID14I10078</h1>
+  </div>
+  
+</div>
+
+<div data-role="page" data-dialog="true" id="p1">
+  <div data-role="header">
+    <h1>Pregunta 1</h1>
+  </div>
+
+  <div data-role="main" class="ui-content">
+    <p>Este estudiante parece estar a gusto en este colegio</p>
+    <a href="#preguntas">Volver</a>
+  </div>
+
+  <div data-role="footer">
+    <h1>Pregunta 1</h1>
+  </div>
+</div>
+
+<div data-role="page" data-dialog="true" id="p2">
+  <div data-role="header">
+    <h1>Pregunta 2</h1>
+  </div>
+
+  <div data-role="main" class="ui-content">
+    <p>Este estudiante demuestra interes por aprender</p>
+    <a href="#preguntas">Volver</a>
+  </div>
+
+  <div data-role="footer">
+    <h1>Pregunta 2</h1>
+  </div>
+</div>
+
+<div data-role="page" data-dialog="true" id="p3">
+  <div data-role="header">
+    <h1>Pregunta 3</h1>
+  </div>
+
+  <div data-role="main" class="ui-content">
+    <p>Este estudiante es reponsable</p>
+    <a href="#preguntas">Volver</a>
+  </div>
+
+  <div data-role="footer">
+    <h1>Pregunta 3</h1>
+  </div>
+</div>
+
+<div data-role="page" data-dialog="true" id="p4">
+  <div data-role="header">
+    <h1>Pregunta 4</h1>
+  </div>
+
+  <div data-role="main" class="ui-content">
+    <p>Este estudiante mantiene relaciones de apoyo y colaboración con sus compañeros</p>
+    <a href="#preguntas">Volver</a>
+  </div>
+
+  <div data-role="footer">
+    <h1>Pregunta 4</h1>
+  </div>
+</div>
+
+<div data-role="page" data-dialog="true" id="p5">
+  <div data-role="header">
+    <h1>Pregunta 5</h1>
+  </div>
+
+  <div data-role="main" class="ui-content">
+    <p>Este estudiante participa activamente en clase (por ejemplo, hace preguntas, opina, ofrece ayuda)</p>
+    <a href="#preguntas">Volver</a>
+  </div>
+
+  <div data-role="footer">
+    <h1>Pregunta 5</h1>
+  </div>
+</div>
+
+<div data-role="page" data-dialog="true" id="p6">
+  <div data-role="header">
+    <h1>Pregunta 6</h1>
+  </div>
+
+  <div data-role="main" class="ui-content">
+    <p>Este estudiante cumple con las normas de convivencia escolar y comportamiento de este colegio</p>
+    <a href="#preguntas">Volver</a>
+  </div>
+
+  <div data-role="footer">
+    <h1>Pregunta 6</h1>
+  </div>
+</div>
+
+<div data-role="page" data-dialog="true" id="p7">
+  <div data-role="header">
+    <h1>Pregunta 7</h1>
+  </div>
+
+  <div data-role="main" class="ui-content">
+    <p>Cuando trabaja en grupo, el/la estudiante suele tomar la iniciativa y organizar a sus compañeros</p>
+    <a href="#preguntas">Volver</a>
+  </div>
+
+  <div data-role="footer">
+    <h1>Pregunta 7</h1>
+  </div>
+</div>
+
+<div data-role="page" data-dialog="true" id="p8">
+  <div data-role="header">
+    <h1>Pregunta 8</h1>
+  </div>
+
+  <div data-role="main" class="ui-content">
+    <p>Este estudiante es persistente con las tareas escolares, aunque éstas sean difíciles</p>
+    <a href="#preguntas">Volver</a>
+  </div>
+
+  <div data-role="footer">
+    <h1>Pregunta 8</h1>
+  </div>
+</div>
+
+<div data-role="page" data-dialog="true" id="p9">
+  <div data-role="header">
+    <h1>Pregunta 9</h1>
+  </div>
+
+  <div data-role="main" class="ui-content">
+    <p>Este estudiante usa distintas estrategias para aprender (por ejemplo, anotar palabras nuevas, hacer mapas conceptuales, resúmenes, esquemas</p>
+    <a href="#preguntas">Volver</a>
+  </div>
+
+  <div data-role="footer">
+    <h1>Pregunta 9</h1>
+  </div>
+</div>
+
+<div data-role="page" data-dialog="true" id="p10">
+  <div data-role="header">
+    <h1>Pregunta 10</h1>
+  </div>
+
+  <div data-role="main" class="ui-content">
+    <p>Este estudiante planifica sus tareas (por ejemplo, plantea objetivos, pasos a seguir, y organiza sus tiempos)</p>
+    <a href="#preguntas">Volver</a>
+  </div>
+
+  <div data-role="footer">
+    <h1>Pregunta 10</h1>
+  </div>
+</div>
+
+<div data-role="page" data-dialog="true" id="p11">
+  <div data-role="header">
+    <h1>Pregunta 11</h1>
+  </div>
+
+  <div data-role="main" class="ui-content">
+    <p>Este estudiante autorregula su aprendizaje (por ejemplo, está atento a posibles errores o incoherencias, monitorea su progreso, busca ayuda cuando no entiende, revisa sus tareas después de terminarlas) </p>
+    <a href="#preguntas">Volver</a>
+  </div>
+
+  <div data-role="footer">
+    <h1>Pregunta 11</h1>
+  </div>
+</div>
+
+<div data-role="page" data-dialog="true" id="p12">
+  <div data-role="header">
+    <h1>Pregunta 12</h1>
+  </div>
+
+  <div data-role="main" class="ui-content">
+    <p>Este estudiante intenta hacer su trabajo a fondo y bien, en lugar de tratar de hacerlo “por cumplir” </p>
+    <a href="#preguntas">Volver</a>
+  </div>
+
+  <div data-role="footer">
+    <h1>Pregunta 12</h1>
+  </div>
+</div>
+
+<div data-role="page" data-dialog="true" id="p13">
+  <div data-role="header">
+    <h1>Pregunta 13</h1>
+  </div>
+
+  <div data-role="main" class="ui-content">
+    <p>Este estudiante es capaz de seguir estudios superiores una vez que termine cuarto medio</p>
+    <a href="#preguntas">Volver</a>
+  </div>
+
+  <div data-role="footer">
+    <h1>Pregunta 13</h1>
+  </div>
+</div>
+
+<div data-role="page" data-dialog="true" id="p14">
+  <div data-role="header">
+    <h1>Pregunta 14</h1>
+  </div>
+
+  <div data-role="main" class="ui-content">
+    <p>Cuando comenzamos una nueva actividad, sé que será capaz de hacerlo bien</p>
+    <a href="#preguntas">Volver</a>
+  </div>
+
+  <div data-role="footer">
+    <h1>Pregunta 14</h1>
+  </div>
+</div>
+
+<div data-role="page" data-dialog="true" id="p15">
+  <div data-role="header">
+    <h1>Pregunta 15</h1>
+  </div>
+
+  <div data-role="main" class="ui-content">
+    <p>Si este estudiante obtuviera una buena calificación se debería sobretodo a su capacidad y a sus propias habilidades</p>
+    <a href="#preguntas">Volver</a>
+  </div>
+
+  <div data-role="footer">
+    <h1>Pregunta 15</h1>
+  </div>
+</div>
+
+<div data-role="page" data-dialog="true" id="p16">
+  <div data-role="header">
+    <h1>Pregunta 16</h1>
+  </div>
+
+  <div data-role="main" class="ui-content">
+    <p>Si este estudiante obtuviera una buena calificación se debería fundamentalmente a su propio interés y motivación</p>
+    <a href="#preguntas">Volver</a>
+  </div>
+
+  <div data-role="footer">
+    <h1>Pregunta 16</h1>
+  </div>
+</div>
+
+<div data-role="page" data-dialog="true" id="p17">
+  <div data-role="header">
+    <h1>Pregunta 17</h1>
+  </div>
+
+  <div data-role="main" class="ui-content">
+    <p>Si este estudiante obtuviera una buena calificación se debería esencialmente a los apoyos recibidos</p>
+    <a href="#preguntas">Volver</a>
+  </div>
+
+  <div data-role="footer">
+    <h1>Pregunta 17</h1>
+  </div>
+</div>
+
+</body>
+</html>
